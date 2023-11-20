335813e10e4a8c30a43774971936c598d5b9b0fb
tuann300724 <tuann300724@gmail.com>
2023-11-20 08:35:17 +0700

    new

--- resources/views/home/contract.blade.php
+++ resources/views/home/contract.blade.php
@@ -2,31 +2,7 @@
 @section('title',"detail_blog")
     
 @section('content')
-<div>
-    <div class="contact">
-        <div class="contact-container">
-            <div class="contact-box">
-                <div class="contact-details">
-                    <img src="images/1.jpg">
-                    <h1>TuanCuTo</h1>
-                    <p>TuanCT@gmail.com</p>
-                </div>
-                <div class="contact-form">
-                    <form>
-                        <span>Contact </span>
-                        <input type="text" placeholder="Full Name">
-                        <input type="text" placeholder="Email">
-                        <input type="text" placeholder="Subject">
-                        <textarea placeholder="Enter Message"></textarea>
-                        <input type="submit" value="Send">
-                    </form>
-                </div>
-    
-            </div>
-        </div>
-    </div>
-</div>
-    
+
 
 
 @endsection
